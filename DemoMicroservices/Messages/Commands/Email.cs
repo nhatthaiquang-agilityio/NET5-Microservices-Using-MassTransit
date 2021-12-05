a8acb67d0251054d563e550b9b755f04e5423594
Nhat Thai <nhat.thaiquang@asnet.com.vn>
2021-12-05 12:12:55 +0700

    Add Message library

---
+++ DemoMicroservices/Messages/Commands/Email.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Messages.Commands
+{
+    public class Email
+    {
+        public Guid EmailId { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public string EmailContent { get; set; }
+    }
+}
---
+++ DemoMicroservices/Messages/Commands/INotification.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Messages.Commands
+{
+    public interface INotification
+    {
+        public Guid NotificationId { get; set; }
+
+        public string NotificationType { get; set; }
+
+        public string NotificationContent { get; set; }
+
+        public DateTime NotificationDate { get; set; }
+    }
+}

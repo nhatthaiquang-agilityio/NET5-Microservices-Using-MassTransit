00c48b4b2e3a76b703034c0c839d25c1c1ef0aa0
Nhat Thai <nhat.thaiquang@asnet.com.vn>
2021-12-05 12:14:34 +0700

    Add Producer

---
+++ DemoMicroservices/Producer/Controllers/NotificationController.cs
@@ -0,0 +1,72 @@
+﻿using MassTransit;
+using Messages.Commands;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Producer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NotificationController : ControllerBase
+    {
+        private readonly ILogger<NotificationController> _logger;
+        private readonly IBus _bus;
+        private readonly IPublishEndpoint _publishEndpoint;
+
+        public NotificationController(ILogger<NotificationController> logger, IBus bus, IPublishEndpoint publishEndpoint)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _bus = bus ?? throw new ArgumentNullException(nameof(bus));
+            _publishEndpoint = publishEndpoint ?? throw new ArgumentNullException(nameof(publishEndpoint));
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            _logger.LogInformation("Get Notification API");
+            return new List<string>();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Notification(Messages.Models.NotificationViewModel notificationModel)
+        {
+            _logger.LogInformation("Post Notification API");
+
+            if (notificationModel != null)
+            {
+                var notify = new
+                {
+                    NotificationId = Guid.NewGuid(),
+                    NotificationType = notificationModel.NotificationType,
+                    NotificationDate = DateTime.Now,
+                    NotificationContent = notificationModel.NotificationContent
+                };
+
+                try
+                {
+                    await _publishEndpoint.Publish<INotification>(notify);
+
+                    _logger.LogInformation(
+                       "Send to a message {NotificationId}, {NotificationType}",
+                       notify.NotificationId, notify.NotificationType);
+
+                    return Ok("Notification is sent.");
+                }
+                catch (Exception exception)
+                {
+                    _logger.LogError(
+                        exception,
+                        "Error Send Notification {NotificationId}, {NotificationType}",
+                        notify.NotificationId, notify.NotificationType);
+                }
+
+
+            }
+
+            return BadRequest();
+        }
+    }
+}

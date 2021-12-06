36c035b333f913e57c71ae42379aeecee51c5e54
Nhat Thai <nhat.thaiquang@asnet.com.vn>
2021-12-06 08:51:13 +0700

    add Notification project

---
+++ DemoMicroservices/Notification/Consumers/EmailConsumer.cs
@@ -0,0 +1,47 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+
+namespace Notification.Consumers
+{
+    public class EmailConsumer : IConsumer<Messages.Commands.INotification>
+    {
+        private readonly ILogger<EmailConsumer> _logger;
+
+        public EmailConsumer(ILogger<EmailConsumer> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public Task Consume(ConsumeContext<Messages.Commands.INotification> context)
+        {
+
+            var data = context.Message;
+
+            _logger.LogInformation(
+                "Consume Notification Message: {NotificationId}, {NotificationType}, {NotificationContent}",
+                data.NotificationId, data.NotificationType, data.NotificationContent);
+
+            try
+            {
+                // TODO: call servive/task
+                Task.Delay(2000);
+
+            }
+            catch (Exception exception)
+            {
+                _logger.LogError(
+                    exception,
+                    "Unable to send Email. CorrelationId {NotificationId}, {NotificationType}, {NotificationContent}",
+                    data.NotificationId, data.NotificationType, data.NotificationContent);
+            }
+
+            _logger.LogInformation("Consumed Order Message");
+
+            return Task.CompletedTask;
+        }
+
+    }
+}
---
+++ DemoMicroservices/Notification/Startup.cs
@@ -0,0 +1,122 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MassTransit;
+using MassTransit.Definition;
+using Messages;
+using Notification.Consumers;
+using RabbitMQ.Client;
+using Serilog;
+using Microsoft.Extensions.Logging;
+
+namespace Notification
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var configuration = new ConfigurationBuilder()
+              .AddJsonFile("appsettings.json")
+              .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
+            services.AddRazorPages();
+            
+            services.AddMassTransit(configureMassTransit =>
+            {
+                configureMassTransit.AddConsumer<PushNotificationConsumer>();
+                configureMassTransit.AddConsumer<EmailConsumer>();
+
+                configureMassTransit.UsingRabbitMq((context, configure) =>
+                {
+                    configure.PrefetchCount = 4;
+
+                    // Ensures the processor gets its own queue for any consumed messages
+                    configure.ConfigureEndpoints(context, new KebabCaseEndpointNameFormatter(true));
+                    ServiceBusConnectionConfig.ConfigureNodes(configuration, configure, "MessageBus");
+
+                    configure.ReceiveEndpoint("notification-message", receive =>
+                    {
+                        // turns off default fanout
+                        receive.ConfigureConsumeTopology = false;
+
+                        // a replicated queue to provide high availability and data safety. available in RMQ 3.8+
+                        receive.SetQuorumQueue();
+
+                        // enables a lazy queue for more stable cluster with better predictive performance.
+                        // Please note that you should disable lazy queues if you require really high performance, if the queues are always short, or if you have set a max-length policy.
+                        receive.SetQueueArgument("declare", "lazy");
+
+                        receive.ConfigureConsumer<PushNotificationConsumer>(context);
+
+                        receive.Bind("send-notification", eventMessage =>
+                        {
+                            eventMessage.RoutingKey = "notification.push";
+                            eventMessage.ExchangeType = ExchangeType.Topic;
+                        });
+                    });
+
+                    configure.ReceiveEndpoint("email-message", receive =>
+                    {
+                        // turns off default fanout
+                        receive.ConfigureConsumeTopology = false;
+
+                        // a replicated queue to provide high availability and data safety. available in RMQ 3.8+
+                        receive.SetQuorumQueue();
+
+                        // enables a lazy queue for more stable cluster with better predictive performance.
+                        // Please note that you should disable lazy queues if you require really high performance, if the queues are always short, or if you have set a max-length policy.
+                        receive.SetQueueArgument("declare", "lazy");
+
+                        receive.ConfigureConsumer<EmailConsumer>(context);
+
+                        receive.Bind("send-notification", eventMessage =>
+                        {
+                            eventMessage.RoutingKey = "notification.email";
+                            eventMessage.ExchangeType = ExchangeType.Topic;
+                        });
+                    });
+
+                });
+            });
+            services.AddMassTransitHostedService();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}

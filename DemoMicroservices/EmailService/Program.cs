cec9995fa58a3a6449d938201c943262aa06dbca
Nhat Thai <nhat.thaiquang@asnet.com.vn>
2021-12-07 13:51:58 +0700

    Add Email Service into the solution

---
+++ DemoMicroservices/EmailService/Program.cs
@@ -0,0 +1,114 @@
+using MassTransit;
+using MassTransit.Definition;
+using Messages;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using EmailService.Consumers;
+using RabbitMQ.Client;
+using Serilog;
+using System.Threading.Tasks;
+using System;
+
+namespace EmailService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var hostBuilder = CreateHostBuilder(args);
+
+            var host = hostBuilder.Build();
+
+            // host.Services.GetRequiredService<IBusControl>().Start();
+
+            Task.Run(async () => await host.RunAsync()).GetAwaiter().GetResult();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+              .AddJsonFile("appsettings.json")
+              .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging =>
+                {
+                    logging.AddSerilog();
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddTransient<EmailService>();
+
+                    services.AddMassTransit(configureMassTransit =>
+                    {
+                        configureMassTransit.AddConsumer<EmailConsumer>();
+
+                        if (Boolean.Parse(configuration["UsingAmazonSQS"]))
+                        {
+                            configureMassTransit.UsingAmazonSqs((context, configure) =>
+                            {
+                                ServiceBusConnectionConfig.ConfigureNodes(configuration, configure, "MessageBusSQS");
+
+                                configure.ReceiveEndpoint(configuration["Queue"], receive =>
+                                {
+                                    // disable the default topic binding
+                                    receive.ConfigureConsumeTopology = false;
+
+                                    receive.ConfigureConsumer<EmailConsumer>(context);
+
+                                    receive.Subscribe(configuration["Bind"], s =>
+                                    {
+                                        // set topic attributes
+                                        s.TopicAttributes["DisplayName"] = configuration["Topic"];
+
+                                        // Using Environment tag
+                                        // s.TopicTags.Add("environment", "dev");
+                                    });
+                                });
+                            });
+                        }
+                        else
+                        {
+                            configureMassTransit.UsingRabbitMq((context, configure) =>
+                            {
+                                configure.PrefetchCount = 4;
+
+                                // Ensures the processor gets its own queue for any consumed messages
+                                configure.ConfigureEndpoints(context, new KebabCaseEndpointNameFormatter(true));
+                                ServiceBusConnectionConfig.ConfigureNodes(configuration, configure, "MessageBus");
+
+                                configure.ReceiveEndpoint(configuration["Queue"], receive =>
+                                {
+                                    // turns off default fanout
+                                    receive.ConfigureConsumeTopology = false;
+
+                                    // a replicated queue to provide high availability and data safety. available in RMQ 3.8+
+                                    receive.SetQuorumQueue();
+
+                                    // enables a lazy queue for more stable cluster with better predictive performance.
+                                    // Please note that you should disable lazy queues if you require really high performance, if the queues are always short, or if you have set a max-length policy.
+                                    receive.SetQueueArgument("declare", "lazy");
+
+                                    receive.ConfigureConsumer<EmailConsumer>(context);
+
+                                    receive.Bind(configuration["Bind"], eventMessage =>
+                                    {
+                                        eventMessage.RoutingKey = configuration["Topic"];
+                                        eventMessage.ExchangeType = ExchangeType.Topic;
+                                    });
+                                });
+
+                            });
+                        }
+                    });
+
+                    services.AddMassTransitHostedService();
+                });
+        }
+    }
+}

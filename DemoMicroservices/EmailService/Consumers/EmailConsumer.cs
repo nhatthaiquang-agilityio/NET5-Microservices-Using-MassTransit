58b28d4247b738a88a42a0174f7cfb1317a967ee
Nhat Thai <nhat.thaiquang@asnet.com.vn>
2021-12-07 13:49:37 +0700

    Add Email Service

--- DemoMicroservices/EmailService/Consumers/EmailConsumer.cs
+++ DemoMicroservices/EmailService/Consumers/EmailConsumer.cs
@@ -1,34 +1,34 @@
-﻿using MassTransit;
+using MassTransit;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Threading.Tasks;
 
 
-namespace Notification.Consumers
+namespace EmailService.Consumers
 {
     public class EmailConsumer : IConsumer<Messages.Commands.INotification>
     {
         private readonly ILogger<EmailConsumer> _logger;
-
-        public EmailConsumer(ILogger<EmailConsumer> logger)
+        private readonly EmailService _emailService;
+        public EmailConsumer(ILogger<EmailConsumer> logger, EmailService emailService)
         {
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _emailService = emailService ?? throw new ArgumentNullException(nameof(emailService));
         }
 
         public Task Consume(ConsumeContext<Messages.Commands.INotification> context)
         {
-
             var data = context.Message;
 
             _logger.LogInformation(
-                "Consume Notification Message: {NotificationId}, {NotificationType}, {NotificationContent}",
-                data.NotificationId, data.NotificationType, data.NotificationContent);
+                "Consume Email Message: {NotificationId}, {NotificationType}, {NotificationContent}, {NotificationAddress}",
+                data.NotificationId, data.NotificationType, data.NotificationContent, data.NotificationAddress);
 
             try
             {
                 // TODO: call servive/task
                 Task.Delay(2000);
-
+                _emailService.SendEmail(data.NotificationId, data.NotificationAddress, data.NotificationContent);
             }
             catch (Exception exception)
             {
@@ -38,7 +38,7 @@ namespace Notification.Consumers
                     data.NotificationId, data.NotificationType, data.NotificationContent);
             }
 
-            _logger.LogInformation("Consumed Order Message");
+            _logger.LogInformation("Consumed Email Message");
 
             return Task.CompletedTask;
         }
